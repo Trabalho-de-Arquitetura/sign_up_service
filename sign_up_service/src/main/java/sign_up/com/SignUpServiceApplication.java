a8467c69b49511150042ae257d44e9021d3739b9
Guilherme Chaves <guilherme.chaves66@gmail.com>
2025-05-23 22:40:04 -0300

    start: starting-project

---
+++ sign_up_service/src/main/java/sign_up/com/SignUpServiceApplication.java
@@ -0,0 +1,13 @@
+package sign_up.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignUpServiceApplication.class, args);
+    }
+
+}
---
+++ sign_up_service/src/test/java/sign_up/com/SignUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sign_up.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
